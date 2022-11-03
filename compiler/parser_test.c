0835fa3f169976e38b8feb4e6c7fec4721124ace
jiakun <jiakun_wang@yeah.net>
2022-11-03 15:24:21 +0000

    测试：模糊框架测试

---
+++ compiler/parser_test.c
@@ -0,0 +1,8 @@
+#include "compiler.h"
+#include <stdio.h>
+
+//gcc -g -Wall parser_test.c parser.c lexer.c ../散列表_unicode切片_编码/散列表_unicode切片_编码.c ../unicode切片/unicode切片.c ../utf8_unicode/utf8_unicode.c && ./a.out
+int main() {
+    输入文件_语法分析 = fopen("parser_test.txt", "r");
+    文件_解析();
+}
