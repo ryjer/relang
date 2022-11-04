e5a925f0f56a6081e0cb82264a18ededd00de2b1
jiakun <jiakun_wang@yeah.net>
2022-11-04 09:43:33 +0000

    测试：插桩测试

--- compiler/parser_test.c
+++ compiler/parser_test.c
@@ -3,6 +3,6 @@
 
 //gcc -g -Wall parser_test.c parser.c lexer.c ../散列表_unicode切片_编码/散列表_unicode切片_编码.c ../unicode切片/unicode切片.c ../utf8_unicode/utf8_unicode.c && ./a.out
 int main() {
-    输入文件_语法分析 = fopen("parser_test.txt", "r");
+    输入文件_语法分析 = fopen("parser_test.go", "r");
     文件_解析();
 }
---
+++ compiler/parser_test.go
@@ -0,0 +1,7 @@
+-const x, y int = 1, 2
+
+var 长度 int = 8
+
+// +proc main() {
+//     fmt.printf("hello, world\n")
+// }
