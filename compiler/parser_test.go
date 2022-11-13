b992b9a36d290229d59ee6101e47463a4968aa59
jiakun <jiakun_wang@yeah.net>
2022-11-13 13:07:34 +0000

    重构：使用通用 三元节点 代替 表达式节点 语句节点

--- compiler/parser_test.go
+++ compiler/parser_test.go
@@ -1,4 +1,5 @@
-add(a+b,-i,x[j],this.age,5,6,7,8,9 的尺寸,10,11,12)
-arr[x+y] += -i + j + add()
--i
-a ∧= 若 -a+b*c > x%y 则 -~!me.age[4]->++-- + 1 else true
+label test: 
+	add(a+b,-i,x[j],this.age,5,6,7,8,9 的尺寸,10,11,12)
+// arr[x+y] += -i + j + add()
+// -i
+// a ∧= 若 -a+b*c > x%y 则 -~!me.age[4]->++-- + 1 else true
