46d0a410fff787ac8c325b4d7cc45485863d1a2e
jiakun <jiakun_wang@yeah.net>
2022-11-13 13:11:27 +0000

    重构：重命名 节点创建函数名

--- compiler/parser_test.go
+++ compiler/parser_test.go
@@ -1,5 +1,4 @@
-label test: 
-	add(a+b,-i,x[j],this.age,5,6,7,8,9 的尺寸,10,11,12)
+label test: add(a+b,-i,x[j],this.age,5,6,7,8,9 的尺寸,10,11,12)
 // arr[x+y] += -i + j + add()
 // -i
 // a ∧= 若 -a+b*c > x%y 则 -~!me.age[4]->++-- + 1 else true
