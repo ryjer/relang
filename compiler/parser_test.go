e645c2da798b4e530f4555d8261411eb0ed1cced
jiakun <jiakun_wang@yeah.net>
2022-11-14 13:31:09 +0000

    +功能：《语句块》解析

--- compiler/parser_test.go
+++ compiler/parser_test.go
@@ -1,15 +1,20 @@
-return 1;
-defer add(x, y)
-continue L1
-break x
-goto test
-label test: 
+{
+	{
+		return
+	}
+	// 注释
+	defer add(x, y)
+	continue L1
+	break x
+	label test:  goto test
+	return 1;
+}
 {
 }
-if a>b {
-	label test:
-}
 add(a+b,-i,x[j],this.age,5,6,7++,8,9 的尺寸,10,11,12)
+// if a>b {
+// 	label test:
+// }
 // arr[x+y] += -i + j + add()
 // -i
 // a ∧= 若 -a+b*c > x%y 则 -~!me.age[4]->++-- + 1 else true
