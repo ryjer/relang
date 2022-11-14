556f01fb336c21d86666a168962f6497a4fd80c6
jiakun <jiakun_wang@yeah.net>
2022-11-14 14:11:44 +0000

    +功能：《如果》语句解析

--- compiler/parser_test.go
+++ compiler/parser_test.go
@@ -1,4 +1,9 @@
 {
+	if a>b {
+		i += 1
+	} else {
+		return i *= 2
+	}
 	_ := 1
 	// 注释
 	defer add(x, y)
@@ -10,9 +15,6 @@
 {
 }
 add(a+b,-i,x[j],this.age,5,6,7++,8,9 的尺寸,10,11,12)
-// if a>b {
-// 	label test:
-// }
 // arr[x+y] += -i + j + add()
 // -i
 // a ∧= 若 -a+b*c > x%y 则 -~!me.age[4]->++-- + 1 else true
